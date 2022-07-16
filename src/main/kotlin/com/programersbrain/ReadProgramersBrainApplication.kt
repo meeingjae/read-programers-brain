a37d2bdb55f05570a707d32a63eca218adb9b725
Minjae Kim <minjae.kim@mytrum.com>
2022-07-16 09:35:12 +0900

    Initial commit

---
+++ src/main/kotlin/com/programersbrain/ReadProgramersBrainApplication.kt
@@ -0,0 +1,11 @@
+package com.programersbrain
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReadProgramersBrainApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReadProgramersBrainApplication>(*args)
+}
---
+++ src/test/kotlin/com/programersbrain/ReadProgramersBrainApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.programersbrain
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReadProgramersBrainApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
